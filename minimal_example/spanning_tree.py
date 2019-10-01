8ae1ee67a250c533ce5650ea3cebd89c74d72a35
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2019-10-01 16:25:09 -0400

    [feat] add min span tree function

---
+++ minimal_example/spanning_tree.py
@@ -0,0 +1,36 @@
+import argparse
+import networkx as nx
+
+def read_graph(composed_graph_file):
+    with open(composed_graph_file) as fo:
+        composed = fo.readlines()
+
+    G = nx.DiGraph()
+
+    edges = [(int(line.split('\t')[0]), int(line.split('\t')[1]), float(line.split('\t')[-1].strip()))\
+            for line in composed[:-1]]
+    nodes = list(set([int(line.split('\t')[0]) for line in composed]))
+
+    G.add_nodes_from(nodes)
+    G.add_weighted_edges_from(edges)
+    return G
+
+def run_min_span_tree():
+    pass
+
+def main():
+    if __name__=="__main__":
+
+        parser = argparse.ArgumentParser(description='Process some integers.')
+        parser.add_argument('-c', '--composed', help='path to a file with composed transducer', required=True)
+        parser.add_argument('-o', '--output', help='path to the output file', default='stdout')
+
+        args = parser.parse_args()
+        
+        path = args.composed
+
+        read_graph(path)
+        
+
+main()
+
