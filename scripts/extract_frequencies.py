713bfb0eb92ea7d584ae0a3583f156a202581635
Nils <nhjortnaes@gmail.com>
2019-10-22 17:02:47 -0400

    functionify frequency extractor

--- scripts/extract_frequencies.py
+++ scripts/extract_frequencies.py
@@ -1,8 +1,10 @@
 from collections import Counter, namedtuple
 
 
-def extract_arc():
-    pass
+def print_freqs(freqs):
+    with open('frequencies.csv', 'w') as f:
+        for t, c in freqs.most_common():
+            print(str(t) + '\t' + str(c), file=f)
 
 #  (dep_POS, arc_label, head_POS)
 Arc = namedtuple('Arc', 'dep_POS, arc_label, head_POS')
@@ -11,30 +13,37 @@ Arc = namedtuple('Arc', 'dep_POS, arc_label, head_POS')
 # (index, word, lemma, pos, og_pos, ignore, head_index, arc_label, enhanced_label, ignore_2)
 Conll = namedtuple('Conll', 'index, word, lemma, pos, og_pos, ignore, head_index, arc_label, enhanced_label, ignore_2')
 
-with open('../ud-treebanks-v2.4/UD_English-EWT/en_ewt-ud-train.conllu',
-          'r', encoding='utf8') as f:
-    sentences = f.read().split('\n\n')
 
-sentences = [[Conll(*x.split('\t')) for x in s.split('\n') if x and not x.startswith('#')]
-             for s in sentences]
-# print(sentences[2])
+def extract_freqs(file):
+    '''
+    Returns a counter of dependency arc frequencies found in file
+    :return: Counter of Dep Arcs
+    '''
+    with open(file ,'r', encoding='utf8') as f:
+        sentences = f.read().split('\n\n')
 
-freqs = Counter()
+    sentences = [[Conll(*x.split('\t')) for x in s.split('\n') if x and not x.startswith('#')]
+                 for s in sentences]
+    # print(sentences[2])
 
-for sentence in sentences:
-    arcs = []
-    for c in sentence:
-        # skip parataxis words
-        if '.' in c.index:
-            continue
-        try:
-            arcs.append(Arc(c.pos, c.arc_label, sentence[int(c.head_index)-1].pos))
-        except ValueError as e:
-            print(sentence)
-            print(e)
-            print(c)
-    freqs += Counter(arcs)
+    freqs = Counter()
 
-with open('frequencies.csv', 'w') as f:
-    for t, c in freqs.most_common():
-        print(str(t) + '\t' + str(c), file=f)
+    for sentence in sentences:
+        arcs = []
+        for c in sentence:
+            # skip parataxis words
+            if '.' in c.index:
+                continue
+            try:
+                arcs.append(Arc(c.pos, c.arc_label, sentence[int(c.head_index)-1].pos))
+            except ValueError as e:
+                print(sentence)
+                print(e)
+                print(c)
+        freqs += Counter(arcs)
+
+    return freqs
+
+if __name__== "__main__":
+    freqs = extract_freqs('../ud-treebanks-v2.4/UD_English-EWT/en_ewt-ud-train.conllu')
+    print_freqs(freqs)
