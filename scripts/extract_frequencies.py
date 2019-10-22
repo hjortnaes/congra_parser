6d2e9002a9b34c0dddc277e333374ccd2df3cbf0
Nils <nhjortnaes@gmail.com>
2019-10-22 16:47:00 -0400

    frequency extractor

--- scripts/extract_frequencies.py
+++ scripts/extract_frequencies.py
@@ -11,20 +11,30 @@ Arc = namedtuple('Arc', 'dep_POS, arc_label, head_POS')
 # (index, word, lemma, pos, og_pos, ignore, head_index, arc_label, enhanced_label, ignore_2)
 Conll = namedtuple('Conll', 'index, word, lemma, pos, og_pos, ignore, head_index, arc_label, enhanced_label, ignore_2')
 
-with open('../ud-treebanks-v2.4/UD_English-EWT/en_ewt-ud-dev.conllu',
+with open('../ud-treebanks-v2.4/UD_English-EWT/en_ewt-ud-train.conllu',
           'r', encoding='utf8') as f:
     sentences = f.read().split('\n\n')
 
 sentences = [[Conll(*x.split('\t')) for x in s.split('\n') if x and not x.startswith('#')]
              for s in sentences]
-print(sentences[2])
+# print(sentences[2])
 
 freqs = Counter()
 
 for sentence in sentences:
     arcs = []
     for c in sentence:
-        arcs.append(Arc(c.pos, c.arc_label, sentence[int(c.head_index)-1].pos))
+        # skip parataxis words
+        if '.' in c.index:
+            continue
+        try:
+            arcs.append(Arc(c.pos, c.arc_label, sentence[int(c.head_index)-1].pos))
+        except ValueError as e:
+            print(sentence)
+            print(e)
+            print(c)
     freqs += Counter(arcs)
 
-print(freqs)
+with open('frequencies.csv', 'w') as f:
+    for t, c in freqs.most_common():
+        print(str(t) + '\t' + str(c), file=f)
